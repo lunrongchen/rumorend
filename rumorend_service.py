ef5b28055efec7bd87ecb13f816921998e231d07
lostleaf <lostleaf@me.com>
2016-11-19 09:58:01 -0600

    Add an example for calling the detection api

--- rumorend_service.py
+++ rumorend_service.py
@@ -6,8 +6,12 @@ app = Flask("rumorend")
 
 @app.route("/rumor_detect_fake", methods=["POST"])
 def rumor_detect_fake():
-    json_query = request.get_json()
-    if "url" not in json_query:
+    query = request.get_json() or request.form
+    if "url" not in query:
         return jsonify(None)
     result = random.randint(0, 100)
     return jsonify(result)
+
+@app.route("/example")
+def example():
+    return app.send_static_file("example.html")
