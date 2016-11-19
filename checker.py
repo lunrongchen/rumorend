a78d598001ccd177c71c60ff4e4af1f952cc2303
LunrongChen <lunrongchen@163.com>
2016-11-19 13:42:42 -0600

    add checker

---
+++ checker.py
@@ -0,0 +1,12 @@
+import requests
+import re
+
+def getResultByWOT(_url):
+	url_pre = "http://api.mywot.com/0.4/public_link_json2?hosts="
+	url_suf = "/&callback=process&key=9af2a6793e479dacfbc52e93fffd5ae39c6b2a5f"
+	url_request = url_pre +  _url + url_suf
+	WOT_result_jsonp = requests.get(url_request)
+	WOT_result_json = re.sub(r'([a-zA-Z_0-9\.]*\()|(\);?$)','',WOT_result_jsonp.text)
+	print WOT_result_json
+
+getResultByWOT("www.example.COM")
--- tweet_crawl.py
+++ tweet_crawl.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import time
+from bs4 import BeautifulSoup
 
 def GetSingleTweetByUrl(_url):
 	url = _url[:8] + "mobile." + _url[8:]
@@ -9,5 +10,24 @@ def GetSingleTweetByUrl(_url):
 	tweet_data = re.sub('''<a href=['"](.*?)['\"].*?(?:<\/a|\/)>''', '', tweet_data[0])
 	return tweet_data
 
+start = time.time()
 url_test = "https://twitter.com/HillaryClinton/status/799043343275753472"
 print GetSingleTweetByUrl(url_test)
+end = time.time()
+print "--------------------"
+print end - start
+
+
+def GetListUrlByGoogleKeywordSearch(_key_word):
+	url_pre = "https://www.google.com/#safe=inactive&q="
+	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	# url = url_pre + _key_word
+	url = "https://www.google.com/#safe=inactive&q=trump"
+	google_html = requests.get(url, headers=headers)
+	# soup = BeautifulSoup(google_html.text, "html.parser")
+	print google_html.text
+	# tweet_data = re.findall('<cite class="(.*)">(.*)<\/cite>', google_html.text)
+	# print tweet_data
+
+# GetListUrlByGoogleKeywordSearch("trump")
+# <cite class="_Rm">https://www.youtube.com/watch?v=JTPohjxJm1M</cite>
