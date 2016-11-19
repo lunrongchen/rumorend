54b8216c8389089cd3169b45d02d67e9a42ff521
LunrongChen <lunrongchen@163.com>
2016-11-19 17:43:20 -0600

    add time sleep

--- checker.py
+++ checker.py
@@ -10,6 +10,7 @@ def getResultByWOT(_url):
             url_suf = "/&callback=process&key=9af2a6793e479dacfbc52e93fffd5ae39c6b2a5f"
             url_request = url_pre +  _url + url_suf
             WOT_result_jsonp = requests.get(url_request)
+            sleep(0.1)
             WOT_result_json = re.sub(r'([a-zA-Z_0-9\.]*\()|(\);?$)', '', WOT_result_jsonp.text)
             return json.loads(WOT_result_json)
         except:
