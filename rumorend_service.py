7feded53173a606b74b459255bf54b5e1e4ccdc3
lostleaf <lostleaf@me.com>
2016-11-19 13:28:10 -0600

    support https

--- rumorend_service.py
+++ rumorend_service.py
@@ -1,7 +1,9 @@
 import random
 
 from flask import Flask, request, jsonify
+from tweet_crawl import GetSingleTweetByUrl
 
+context = ("cert/server.crt", "cert/server.key")
 app = Flask("rumorend")
 
 @app.route("/rumor_detect_fake", methods=["POST"])
@@ -15,3 +17,18 @@ def rumor_detect_fake():
 @app.route("/example")
 def example():
     return app.send_static_file("example.html")
+
+@app.route("/rumor_detect", methods=["POST"])
+def rumor_detect():
+    query = request.get_json() or request.form
+    if "url" not in query:
+        return jsonify(None)
+    url = query["url"]
+    tweet = GetSingleTweetByUrl(url)
+
+def main():
+    app.run(host="0.0.0.0", port=5000, debug=True, ssl_context=context)
+
+if __name__ == '__main__':
+    main()
+    
