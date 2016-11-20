a0dcb37ec8eeaa3db0735776a6f054ab4b0fdc7d
LunrongChen <lunrongchen@163.com>
2016-11-19 21:18:10 -0600

    add ulrs

--- tweet_crawl.py
+++ tweet_crawl.py
@@ -5,6 +5,7 @@ from time import sleep
 import json
 from bs4 import BeautifulSoup
 
+
 def GetSingleTweetByUrl(_url):
     url = _url[:8] + "mobile." + _url[8:]
     tweet_user = _url.split('/')[3]
@@ -24,8 +25,8 @@ def GetSingleTweetByUrl(_url):
             result['urls'].append(r.url)
         except:
             pass
-    # result = json.dumps(result)
     return result
+
 
 def GetUserScoreById(_user):
     identity_url_pre = "http://api.klout.com/v2/identity.json/twitter?screenName="
@@ -42,28 +43,32 @@ def GetUserScoreById(_user):
     score_info_json = json.loads(score_info_text)
     return score_info_json['score']
 
-# start = time.time()
-# url_test = "https://twitter.com/HillaryClinton/status/799043343275753472"
-# print GetSingleTweetByUrl(url_test)
-# end = time.time()
-# print "--------------------"
-# print end - start
 
 def GetListUrlByGoogleKeywordSearch(_key_word):
-    url_pre = "https://www.google.com/#safe=inactive&q="
+    url_pre = "https://www.google.com/search?q="
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    # url = url_pre + _key_word
-    url = "https://www.google.com/#safe=inactive&q=trump"
+    exclude_site = " -site:twitter.com -site:www.twitter.com"
+    url = url_pre + _key_word + exclude_site + "&num=20"
     google_html = requests.get(url, headers=headers)
-    # soup = BeautifulSoup(google_html.text, "html.parser")
-    print google_html.text
-    # tweet_data = re.findall('<cite class="(.*)">(.*)<\/cite>', google_html.text)
-    # print tweet_data
+    google_urls = re.findall('''<a href=['"](.*?)['\"].*?(?:<\/a|\/)>''', google_html.text)
+    result = set()
+    for google_url in google_urls:
+    	domain_splits = google_url.split('/')
+    	if domain_splits[0] == 'https:' or domain_splits[0] == 'http:':
+    		result.add(domain_splits[2])
+    try:
+    	result.remove("www.google.com")
+        result.remove("support.google.com")
+    except:
+    	pass
+    return result
+
 
 if __name__ == '__main__':
     # url_test = "https://twitter.com/HillaryClinton/status/796056175179599872"
     # url_test = "https://twitter.com/HillaryClinton/status/799043343275753472"
     # print GetSingleTweetByUrl(url_test)
-    GetUserScoreById("HillaryClinton")
+    # GetUserScoreById("HillaryClinton")
+    print GetListUrlByGoogleKeywordSearch("trump")
 
 
